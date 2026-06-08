072ffaea2667e175f29e61bac3b35c1773af0d70
Guilherme Franco <guilherme.franco.0013@gmail.com>
2026-06-07 23:10:59 -0300

    Fix: Test in flow pipeline DEV (endpoint response)

--- app/api/routes.py
+++ app/api/routes.py
@@ -15,9 +15,9 @@ async def root():
 
     return {
         "status": "ok",
-        "message": "Service is healthy",
+        "message": "Service is pipeline DEV is healthy",
         "service": "challenge-devops",
-        "version": "1.0.1",
+        "version": "1.0.0",
         "uptime": "0d 0h 0m",
         "environment": APP_ENV,
     }
@@ -30,9 +30,9 @@ async def health():
 
     return {
         "status": "ok",
-        "message": "Service is healthy",
+        "message": "Service is pipeline DEV is healthy",
         "service": "challenge-devops",
-        "version": "1.0.1",
+        "version": "1.0.0",
         "uptime": "0d 0h 0m",
         "environment": APP_ENV,
     }
